af18e53edf06ff74d6b8582f59ff453935a2adc4
Shikhar Bansal <shikharbansal007@gmail.com>
2023-02-14 10:40:56 +0530

    Chapter4: Completed the search func and Hooks unds

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,67 @@
+import { useState } from "react";
 import { restaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard";
+
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+  return filterData;
+}
 
 // Body Component for body section: It contain all restaurant cards
 // We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
 const Body = () => {
+  // local variable in javascript
+  // const searchTxt = "KFC";
+
+  // searchText is a local state variable in REACT
+  // returns = [variable name, fuunction to update the variable]
+  const [searchText, setSearchText] = useState(""); // to create variables
+  // const [searchClick, setSearchClicked] = useState("False");
+  const [restaurants, setRestaurants] = useState(restaurantList);
+
   return (
-    <div className="restaurant-list">
-      {restaurantList.map((restaurant) => {
-        return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-text"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        {/* <h1>{searchClick}</h1> */}
+        {/* <button
+          className="search-btn"
+          onClick={() => { 
+            setSearchClicked("True");
+          }}
+        >
+          Search
+        </button> */}
+        <button
+          className="search-btn"
+          onClick={() => {
+            //need to filter the data
+            const data = filterData(searchText, restaurants);
+            // update the state - restaurants
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
